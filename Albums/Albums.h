9acd8d6103a3f97d86b91b7986a6e4ef98fc7b6f
Himanshu Tantia <htantia@ebay.com>
2014-10-13 14:52:33 -0700

    Initial Commit

---
+++ Albums/Albums.h
@@ -0,0 +1,13 @@
+//
+//  Albums.h
+//  Albums
+//
+//  Created by Himanshu Tantia on 10/13/14.
+//  Copyright (c) 2014 Himanshu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Albums : NSObject
+
+@end
---
+++ Albums/Albums.m
@@ -0,0 +1,13 @@
+//
+//  Albums.m
+//  Albums
+//
+//  Created by Himanshu Tantia on 10/13/14.
+//  Copyright (c) 2014 Himanshu. All rights reserved.
+//
+
+#import "Albums.h"
+
+@implementation Albums
+
+@end
